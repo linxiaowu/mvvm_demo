77fc1b0f0f9efc1e56bcbcd08cdf5a8aab468c65
linxiaowu5 <lin.l@hcp.group>
2019-03-07 14:23:47 +0800

    添加数据和视图绑定的关系

--- app/src/main/java/com/chutzpah/mvvmdemo/view/LoginActivity.java
+++ app/src/main/java/com/chutzpah/mvvmdemo/view/LoginActivity.java
@@ -1,16 +1,16 @@
 package com.chutzpah.mvvmdemo.view;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
-import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.chutzpah.mvvmdemo.R;
 import com.chutzpah.mvvmdemo.databinding.ActivityLoginBinding;
+import com.chutzpah.mvvmdemo.vm.LoginVM;
 
 /**
- *
  * @author xiaowu
  * @date 2019/03/07
  */
@@ -20,8 +20,16 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityLoginBinding  loginBinding =
+        ActivityLoginBinding loginBinding =
                 DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+        //指定当前的活动生命周期的所有者。
+        loginBinding.setLifecycleOwner(this);
+
+
+        LoginVM loginVM = ViewModelProviders.of(this).get(LoginVM.class);
+
+        loginBinding.setVm(loginVM);
 
 
 
--- app/src/main/java/com/chutzpah/mvvmdemo/vm/LoginVM.java
+++ app/src/main/java/com/chutzpah/mvvmdemo/vm/LoginVM.java
@@ -1,15 +1,44 @@
 package com.chutzpah.mvvmdemo.vm;
 
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.util.Log;
 
 /**
- * Created by xiaowu on 2019/03/07.
+ * @author xiaowu
+ * @date 2019/03/07
+ * 登录页的 viewModel
  */
 public class LoginVM extends ViewModel {
 
+    private String TAG = LoginVM.class.getName();
+
+    public final MutableLiveData<String> userName;
+
+    public final MutableLiveData<String> password;
+
+    public LoginVM() {
+        this.userName = new MutableLiveData<>();
+        this.password = new MutableLiveData<>();
+    }
 
 
+    //登录
+    public void onLogin() {
+        Log.d(TAG, userName.getValue());
+        Log.d(TAG, password.getValue());
+
+        //进行网络请求校验
+
+    }
 
 
+    @Override
+    protected void onCleared() {
+
+        Log.d(TAG, "onCleard");
+
+        super.onCleared();
+    }
 
 }
